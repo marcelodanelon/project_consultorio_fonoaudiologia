a69148de8f6a416e6df33a2ed040741fae34497e
marcelodanelon <marcelodanelon11@gmail.com>
2023-05-31 19:28:09 -0300

    incluir paginação na pesquisa

--- home/views/views_client.py
+++ home/views/views_client.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from home.models import ClientModel
 from django.db.models import Q
 from datetime import datetime
+from django.core.paginator import Paginator
 
 def isDate(var):
     try:
@@ -22,11 +23,15 @@ def index(request):
     )
 
 def listClient(request):
-    clients = ClientModel.objects.all()
+    clients = ClientModel.objects.all().order_by('id')
+
+    paginator = Paginator(clients, 15)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-            'clients': clients,
             'title':'Pesquisa',
+            'page_obj': page_obj,
     }
 
     return render(
@@ -38,24 +43,26 @@ def listClient(request):
 def searchClient(request):
     search_client = request.GET.get('q','').strip()
 
-    print(type(search_client))
     if search_client == "":
         return redirect('home:listClient')
 
     if search_client.isdigit():
-        clients = ClientModel.objects.filter(document1=int(search_client))
+        clients = ClientModel.objects.filter(document1=int(search_client)).order_by('id')
     elif isDate(search_client):
-        clients = ClientModel.objects.filter(born=datetime.strptime(search_client, '%d/%m/%Y').date())
+        clients = ClientModel.objects.filter(born=datetime.strptime(search_client, '%d/%m/%Y').date()).order_by('id')
     else:        
         clients = ClientModel.objects.filter(
             Q(first_name=search_client) | 
             Q(last_name=search_client)
-        )
+        ).order_by('id')
 
+    paginator = Paginator(clients, 15)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-            'clients': clients,
             'title':'Pesquisa',
+            'page_obj': page_obj,
     }
 
     return render(
