596af7406b18546982923b5e63ec283f060a38af
marcelodanelon <marcelodanelon11@gmail.com>
2023-07-10 06:58:29 -0300

    update atendimento 1.0

--- atendimento/views/atendimento/views.py
+++ atendimento/views/atendimento/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from atendimento.forms import AtendimentoForm, AnamneseFormSet
 from home.models import ClientModel
+from atendimento.models import AtendimentoModel
 
 def atendimento(request):
     form_action = reverse('atendimento:atendimento')
     formAnamSet = AnamneseFormSet(request.GET or None)
 
     if request.method == 'POST':
-        form = AtendimentoForm(request.POST)
-        formAnamSet = AnamneseFormSet(request.POST)
-        
+        updateAtend = request.POST.get('updateAtendimento', 0)
+        if updateAtend == 0:
+            form = AtendimentoForm(request.POST)
+            formAnamSet = AnamneseFormSet(request.POST)
+        else:
+            #verificar
+            search = request.POST.get('searchClient')
+            atendimento = AtendimentoModel.objects.get(aClient=search)
+            form = AtendimentoForm(request.POST, instance=atendimento)
+
         if form.is_valid():
             obj1 = form.save()
-            print(formAnamSet)
             if formAnamSet.is_valid():
                 for formA in formAnamSet:
                     obj2 = formA.save(commit=False)
@@ -41,8 +48,17 @@ def dadosClient(request):
 
     search = request.GET.get('searchClient')
     client = ClientModel.objects.filter(pk=search)
-
-    form = AtendimentoForm()
+    try:
+        atendimento = AtendimentoModel.objects.get(aClient=search)
+        if atendimento.aSituaca != 'Concluído':
+            updateAtend = 1
+            form = AtendimentoForm(instance=atendimento)
+        else:
+            form = AtendimentoForm()
+            updateAtend = 0
+    except AtendimentoModel.DoesNotExist:
+        form = AtendimentoForm()
+        updateAtend = 0
 
     context = {
         'client': client,
@@ -51,6 +67,7 @@ def dadosClient(request):
         'name_module': 'Atendimento',
         'form_action': form_action,
         'title': 'Atendimento',
+        'updateAtend': updateAtend,
     }
 
     return render(
