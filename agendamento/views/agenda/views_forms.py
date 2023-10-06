e8e36bbc7ecbfaa8a12f66a6ca1eec26a71ca163
marcelodanelon <marcelodanelon11@gmail.com>
2023-10-06 17:10:03 -0300

    Criação de validação para agenda já existente

--- agendamento/views/agenda/views_forms.py
+++ agendamento/views/agenda/views_forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from agendamento.forms import AgendaForm
 from agendamento.models import AgendaModel
+from django.db.models import Q
 
 @login_required(login_url='home:loginUser')
 def createAgenda(request):
@@ -13,9 +14,27 @@ def createAgenda(request):
     if request.method == 'POST':
         form = AgendaForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Agenda criada com sucesso!')
-            return redirect('agendamento:listAgenda')
+            model = form.save(commit=False)
+            agendas_conflitantes = AgendaModel.objects.filter(
+                Q(aProfessional=model.aProfessional) &
+                Q(aLocal=model.aLocal) &
+                (
+                    (Q(agDatIni__lte=model.agDatIni) & Q(agDatFim__gte=model.agDatIni)) |  
+                    (Q(agDatIni__lte=model.agDatFim) & Q(agDatFim__gte=model.agDatFim)) |  
+                    (Q(agDatIni__gte=model.agDatIni) & Q(agDatFim__lte=model.agDatFim))    
+                ) &
+                (
+                    (Q(agHorIni__lte=model.agHorIni) & Q(agHorFim__gte=model.agHorIni)) |  
+                    (Q(agHorIni__lte=model.agHorFim) & Q(agHorFim__gte=model.agHorFim)) |  
+                    (Q(agHorIni__gte=model.agHorIni) & Q(agHorFim__lte=model.agHorFim))    
+                )
+            )
+            if agendas_conflitantes.exists():
+                messages.error(request, 'Agenda com período já existente!')
+            else:
+                ##model.save()
+                messages.success(request, 'Agenda criada com sucesso!')
+                return redirect('agendamento:listAgenda')
 
     context = {
         'form': form,
