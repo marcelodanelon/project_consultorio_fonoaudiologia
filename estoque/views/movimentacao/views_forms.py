58cb6384c5907b8019e67ae6762b33ce7ab4cc9b
marcelodanelon <marcelodanelon11@gmail.com>
2023-11-24 13:58:08 -0300

    Inclusão de view de Saída de Insumos

--- estoque/views/movimentacao/views_forms.py
+++ estoque/views/movimentacao/views_forms.py
@@ -351,14 +351,151 @@ def createMovimentacaoInsumo(request):
     modelMovimentacao = MovimentacaoInsumoModel()
     formMovimentacao = MovimentacaoInsumoForm()
     formSetMovimentacao=inlineformset_factory(MovimentacaoInsumoModel, ItensMovimentacaoInsumoModel, form=ItensMovimentacaoInsumoForm, extra=0, can_delete=True, min_num=1)
+    formSetMovimentacaoInsumo=inlineformset_factory(MovimentacaoInsumoModel, ItensInsumoModel, form=ItensInsumoForm, extra=0, can_delete=True, min_num=1)
+
 
     if request.method == 'POST':
-        formMovimentacao = MovimentacaoInsumoForm(request.POST)
-        formSetMovimentacao_P = formSetMovimentacao(request.POST, instance=modelMovimentacao)
-        if formMovimentacao.is_valid() and formSetMovimentacao_P.is_valid():
-            ...
+        formMovimentacao = MovimentacaoInsumoForm(request.POST, instance=modelMovimentacao)
+        formSetItensMovimentacao_P = formSetMovimentacao(request.POST, instance=modelMovimentacao)
+        formSetItensInsumo_P = formSetMovimentacaoInsumo(request.POST, instance=modelMovimentacao)
+
+        if formMovimentacao.is_valid() and formSetItensMovimentacao_P.is_valid():
+            match formMovimentacao.cleaned_data['operacao']:
+                case 'Entrada': 
+                    success = True
+                    model = formMovimentacao.save()
+                    modelSet = formSetItensMovimentacao_P.save(commit=False)
+                    modelSetInsumo = formSetItensInsumo_P.save(commit=False)
+
+                    for item in modelSet:
+                        item.local = model.local
+                        item.dataEntrada = model.data
+                    # replica para o Itens no cadastro do Insumo | valores e quantidades serão ajustados mais abaixo
+                    for item in modelSet:
+                        novo_item_insumo = ItensInsumoModel(
+                            movimentacao=item.movimentacao,  
+                            insumo=item.insumo,
+                            valorUnitario=item.valorUnitario,  
+                            valorTotal=item.valorTotal,  
+                            quantidade=item.quantidade,
+                            dataValidade=item.dataValidade,  
+                            dataEntrada=item.dataEntrada,  
+                            serie=item.serie,  
+                            local=item.local,  
+                        )
+                        modelSetInsumo.append(novo_item_insumo)
+
+                    insumo = None
+                    # verifica se há lote existente
+                    for item in modelSet:
+                        try:
+                            insumo = ItensMovimentacaoInsumoModel.objects.filter(insumo=item.insumo.pk).filter(serie=item.serie).filter(local=item.local).get()
+                            insumo_valorUnitario = float(insumo.valorUnitario.replace("R$", "").replace(".", "").replace(",", ".").strip())
+                            insumo_valorUnitario = format_currency(insumo_valorUnitario, 'BRL', locale='pt_BR')
+                        except Exception as e:
+                            insumo = None
+                            print(e)
+
+                        # retira pontos e virgulas e converte em reais
+                        item_valorUnitario = float(item.valorUnitario.replace("R$", "").replace(".", "").replace(",", ".").strip())           
+                        item_valorUnitario = format_currency(item_valorUnitario, 'BRL', locale='pt_BR')
+
+                        # verifica valor unitario divergente
+                        if insumo != None and item_valorUnitario != insumo_valorUnitario:
+                            messages.error(request, f'Valor Unitário para o {insumo}, diferente da série/lote existente.')
+                            success = False
+                            break
+
+                    if success == True:
+                        for item in modelSet:
+                            # controle inclusão de entrada para lote existente ou novo 
+                            if insumo:                    
+                                totalQuantidade = insumo.quantidade + item.quantidade
+                                valorTotal_insumo = float(insumo.valorTotal.replace("R$", "").replace(".", "").replace(",", ".").strip())
+                                valorTotal_item = float(item.valorTotal.replace("R$", "").replace(".", "").replace(",", ".").strip())
+                                totalValor = valorTotal_insumo + (valorTotal_item/100)
+                                totalValor = format_currency(totalValor, 'BRL', locale='pt_BR')
+                                ItensMovimentacaoInsumoModel.objects.filter(insumo=item.insumo.pk).update(quantidade=totalQuantidade)
+                                ItensMovimentacaoInsumoModel.objects.filter(insumo=item.insumo.pk).update(valorTotal=totalValor)
+                                messages.success(request, f'Lote para {insumo} já existente, adicionado quantidade na Data de Entrada: {insumo.dataEntrada}')
+                            else:
+                                item_valorUnitario = float(item.valorUnitario.replace(",","").replace(".",""))
+                                item.valorUnitario = format_currency(item_valorUnitario / 100, 'BRL', locale='pt_BR')
+                                item_valorTotal = float(item.valorTotal.replace(",","").replace(".",""))
+                                item.valorTotal = format_currency(item_valorTotal / 100, 'BRL', locale='pt_BR')
+                                item_valorCompra = float(item.valorCompra.replace(",","").replace(".",""))
+                                item.valorCompra = format_currency(item_valorCompra / 100, 'BRL', locale='pt_BR')
+                                item_valorUnitario = format_currency(item_valorUnitario, 'BRL', locale='pt_BR')
+                                item.save()
+
+                        for item in modelSetInsumo:
+                            # controle inclusão de entrada para lote existente ou novo 
+                            if insumo:                    
+                                totalQuantidade = insumo.quantidade + item.quantidade
+                                valorTotal_insumo = float(insumo.valorTotal.replace("R$", "").replace(".", "").replace(",", ".").strip())
+                                valorTotal_item = float(item.valorTotal.replace("R$", "").replace(".", "").replace(",", ".").strip())
+                                totalValor = valorTotal_insumo + (valorTotal_item/100)
+                                totalValor = format_currency(totalValor, 'BRL', locale='pt_BR')
+                                ItensInsumoModel.objects.filter(insumo=item.insumo.pk).update(quantidade=totalQuantidade)
+                                ItensInsumoModel.objects.filter(insumo=item.insumo.pk).update(valorTotal=totalValor)
+                                messages.success(request, f'Lote para {insumo} já existente, adicionado quantidade na Data de Entrada: {insumo.dataEntrada}')
+                            else:
+                                item_valorUnitario = float(item.valorUnitario.replace(",","").replace(".",""))
+                                item.valorUnitario = format_currency(item_valorUnitario / 100, 'BRL', locale='pt_BR')
+                                item_valorTotal = float(item.valorTotal.replace(",","").replace(".",""))
+                                item.valorTotal = format_currency(item_valorTotal / 100, 'BRL', locale='pt_BR')
+                                item_valorUnitario = format_currency(item_valorUnitario, 'BRL', locale='pt_BR')
+                                item.save()
+                        messages.success(request, 'Movimentação gravada com sucesso!')
+                        return redirect('estoque:index')
+                    else:
+
+                        print(formSetItensMovimentacao_P.errors)
+                        print(formSetItensInsumo_P.errors)
+                case 'Saída':
+                    success=None
+                    model = formMovimentacao.save(commit=False)
+                    modelSet = formSetItensMovimentacao_P.save(commit=False)
+
+                    for item in modelSet:
+                        item.local = model.local
+                        item.dataEntrada = model.data
+
+                    # verifica se há saldo para saída
+                    for item in modelSet:
+                        if success != False:
+                            insumo = ItensInsumoModel.objects.filter(insumo=item.insumo.pk).filter(local=item.local).filter(serie=item.serie).get()                   
+                            if insumo.quantidade >= item.quantidade:
+                                success=True
+                            else:
+                                success=False
+                                break
+
+                    # verifica se informou um usuário caso tipo seja venda
+                    if model.eClient == None and model.tipoMovimentacao == 'venda':
+                        messages.error(request, 'Usuário de venda não informado!')
+                        success = False
+
+                    # realiza ação e verificação
+                    if success:
+                        formMovimentacao.save()
+                        for item in modelSet:
+                            insumo = ItensInsumoModel.objects.filter(insumo=item.insumo.pk).filter(serie=item.serie).filter(local=item.local).get() 
+                            totalQuantidade = insumo.quantidade - item.quantidade
+                            valorTotal_insumo = float(insumo.valorTotal.replace("R$", "").replace(".", "").replace(",", ".").strip())
+                            valorTotal_item = float(item.valorTotal.replace("R$", "").replace(".", "").replace(",", ".").strip())
+                            totalValor = valorTotal_insumo - (valorTotal_item/100)
+                            totalValor = format_currency(totalValor, 'BRL', locale='pt_BR')
+                            ItensInsumoModel.objects.filter(insumo=item.insumo.pk).filter(serie=item.serie).update(quantidade=totalQuantidade)
+                            ItensInsumoModel.objects.filter(insumo=item.insumo.pk).filter(serie=item.serie).update(valorTotal=totalValor)
+                            item.save()
+                        messages.success(request, f'Saída gravada com sucesso!')
+                        return redirect('estoque:index')
+                    else:
+                        messages.error(request, f'Saldo insuficiente para insumo {insumo}!')
         else:
-            print(formSetMovimentacao_P.errors)
+            print(formMovimentacao.errors)
+            print(formSetItensMovimentacao_P.errors)
 
     context = {
         'title': 'Estoque',
